9ecf15cf9f1e3b2a258adfb4e90de2703e13b3b9
beroy <benjamin.roy69@gmail.com>
2024-02-02 15:59:45 +0100

    Initial Push

---
+++ minitalk.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beroy <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 15:53:45 by beroy             #+#    #+#             */
+/*   Updated: 2024/02/02 15:57:12 by beroy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+# include <signal.h>
+
+// ft_printf
+
+int		ft_printf(const char *str, ...);
+
+// ft_printf Utils
+
+ssize_t	ft_putchar(int c);
+ssize_t	ft_putstr(char *str);
+void	ft_putnbr_base(ssize_t nbr, char *str, ssize_t *length);
+void	ft_putnbr_ul(size_t nbr, char *str, ssize_t *length);
+
+#endif
